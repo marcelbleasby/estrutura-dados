bb39045fdc1cef77088496c8732b450f979144b6
Marcel Bastos Bleasby <marcel917@gmail.com>
2025-06-04 09:10:08 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Item {
+    int codigo_produto;
+    int tipo_produto;
+    int descricao_produto;
+    float preco_produto;
+}Item
+
+typedef struct Pilha {
+    int codigo_caixa;
+    
+}
