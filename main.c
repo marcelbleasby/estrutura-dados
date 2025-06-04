dbc9f329a0d71c387d6d606f6d0a9e085785c01d
Marcel Bastos Bleasby <marcel917@gmail.com>
2025-06-04 12:30:45 +0000

    update main.c

--- main.c
+++ main.c
@@ -6,9 +6,27 @@ typedef struct Item {
     int tipo_produto;
     int descricao_produto;
     float preco_produto;
-}Item
+}Item;
 
-typedef struct Pilha {
-    int codigo_caixa;
-    
-}
+typedef struct NoCodigoProduto {
+    int codigo_produto;
+    struct NoCodigoProduto *proximo;
+} NoCodigoProduto;
+
+typedef struct Caixa {
+    int codigo_caixa;                      
+    NoCodigoProduto *lista_codigos_produtos; 
+    int quantidade_unidades_na_caixa;      
+    int capacidade_maxima;
+} Caixa;
+
+typedef struct NoCaixa {
+    Caixa caixa_data;
+    struct NoCaixa *proximo;
+} NoCaixa;
+
+typedef struct PilhaCaixas {
+    NoCaixa *topo;
+    int tipo_produto_pilha;
+    int capacidade_caixa_pilha;
+} PilhaCaixas;
